7869da27cba7c7a7e4dd3f4e2b639cc18a58fb2d
caioplima <caioplima@hotmail.com>
2020-07-06 19:31:59 -0300

    Javascript Course - Version 1.0

---
+++ exercicios-js/Fundamentos/VariaveisEConstantes.js
@@ -0,0 +1,8 @@
+var a = 30
+let b = 40
+console.log(a, b)
+
+//o let não permite que a variavel seja declarada 2x.
+var a = 10
+b = 5
+console.log(a,b)
---
+++ exercicios-js/Fundamentos/string.js
@@ -0,0 +1,10 @@
+const escola = "cod3r"
+
+
+console.log(escola.charAt(4)) //mostra o caractere posição 4
+console.log(escola.charAt(5)) //mostra o caractere posição 5
+console.log(escola.indexOf(3)) //encontra a posição do carctere
+console.log(escola.substring(1)) //mostra caracteres depois da posição
+console.log(escola.substring(0,3)) //mostra caracteres da posição zero até a posição 2 (ex)
+console.log('Escola: ' + escola + "!") //concatenando String
+console.log(escola.replace(3,'e')) //trocar caractere
